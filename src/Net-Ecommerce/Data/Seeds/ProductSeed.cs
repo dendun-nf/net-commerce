7af2ab313ad96200ea45d01775559a1b12c97fb8
dendun-nf <deni.rianto0308@gmail.com>
2023-11-16 06:16:52 +0700

    feat: change seeding condition and product seed

---
+++ src/Net-Ecommerce/Data/Seeds/ProductSeed.cs
@@ -0,0 +1,21 @@
+using Net_Ecommerce.Features.Products;
+
+namespace Net_Ecommerce.Data.Seeds;
+
+public static class ProductSeed
+{
+    public static IEnumerable<Product> Products => new List<Product>
+    {
+        new("stems", "massage doctor bound bit gain daily running gave last suit one review good class sitting main apart number anyone measure tonight fifth trail wheat", 77, 96.5297m),
+        new("quickly", "short arrange teach mysterious rush brief fair sense stage motion drove rope shake fallen cage voyage public train loose teeth impossible lips time eight", 69, 98.2222m),
+        new("flies", "bear available road discover snake chair together president broken writer distant light key upward fast hill end nearby will basic protection noon spoken ruler", 85, 75.5734m),
+        new("within", "his change determine cloth caught season tongue cookies muscle gold everywhere sheet month plane party ought stiff onto drop take location allow does egg", 24, 21.4643m),
+        new("automobile", "needle chair divide student mix base card movie eat distance fog type hang cook eaten beat nearly whatever pressure push cold some voice program", 59, 35.6153m),
+        new("swam", "solution road tales twelve cannot exchange exist word bare flame on paint circle nose spring simple pitch piece train behavior fur ground higher frame", 81, 91.4485m),
+        new("movement", "hour cutting worker large express surrounded track why simplest most wing earn rabbit factory sky outside package throat final audience nobody yet glad cave", 67, 79.2593m),
+        new("citizen", "expect picture night pictured some worried avoid troops about well judge swept on mail useful religious flight laugh simplest zero order solution brief practical", 68, 74.2153m),
+        new("smell", "sum stick choose swim mother compass pull former general difficult again organization familiar immediately place open eaten feed ran rule bank space drove cut", 66, 34.732m),
+        new("twelve", "swam condition quite solve close large ten interior me excited ants feathers law handsome whose green drive someone nature drink lesson happily energy dirty", 62, 46.9641m),
+        new("pony", "widely hurt influence system previous forgotten done up stranger parallel desk equal connected factory understanding warm selection got milk sight bottle replace ear familiar", 86, 69.5489m),
+    };
+}
